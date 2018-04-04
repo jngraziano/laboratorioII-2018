3c5338bb61f35c83e3f8c72ef7abf3a7cc52efea
jngraziano <jngraziano@gmail.com>
2018-04-03 22:10:45 -0300

    EjercicioClase5 terminado pero con errores. Pasa la primer carga.

---
+++ EjercicioClase5/Estante.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase5
+{
+    class Estante
+    {
+        #region Variables constructores y getters
+
+        private Producto[] productos;
+        private int ubicacionEstante;
+        
+
+        private Estante(int capacidad)
+        {
+            
+            this.productos = new Producto[capacidad];
+            
+            //if (object.ReferenceEquals(aux, null))
+            //{
+                //for (int i = 0; i < capacidad; i++)
+                //{
+                //    this.productos[i] = aux[i];
+                //}
+            //}                      
+        }
+        public Estante(int capacidad, int ubicacion)
+            : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        public Producto[] GetProducto { get { return this.productos; } }
+                
+        #endregion
+
+        #region Metodos y Sobrecargas
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Ubicacion del estante: ");
+            sb.AppendLine(e.ubicacionEstante.ToString());
+            //si hay ganas, usar el metodo MostrarProducto() en vez de esto
+            foreach (var item in e.GetProducto)
+            {
+                sb.AppendLine("Producto: ");
+                sb.AppendLine(Producto.MostrarProducto(item));
+
+                //Otra forma usando
+                //conversion explicita del producto (PROBAR DSP)
+                //sb.Append("Marca: ");
+                //sb.AppendLine(item.GetMarca);
+                //sb.Append("Codigo de Barras: ");
+                
+                //sb.AppendLine((string)item);
+                //sb.Append("Precio: ");
+                //sb.AppendLine(item.GetPrecio.ToString());
+                                                
+            }
+
+            return sb.ToString();
+
+        }
+
+        #region Sobrecargas
+        public static bool operator ==(Estante e, Producto p)
+        {
+            //Igualdad, retornará true, si es que el producto ya se encuentra 
+            //en el estante, false, caso contrario.
+            bool devuelve=false;
+
+            for (int i = 0; i < e.GetProducto.Length; i++)
+            {
+                if (object.ReferenceEquals(e.GetProducto[i], null) || e.GetProducto[i] != p)
+                {
+                    devuelve = true;
+                    break;
+
+                }
+                
+                
+            }
+            
+            return devuelve;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        { return !(e == p); }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            /*Adición, retornará true y agregará el producto si 
+            el estante posee capacidad de almacenar al menos un producto 
+            más y dicho producto no se encuentra en él; false, 
+            caso contrario. Reutilizar código*/
+            bool devuelve=false;
+           
+            for (int i = 0; i < e.GetProducto.Length; i++)
+			{
+
+                if (e != p)
+                {
+                    
+
+                    if (Object.ReferenceEquals(e.GetProducto[i], null))
+                    {
+                    //Significa que primero me fijo Si el producto no esta ya en
+                    //los productos ya guardados, asigna true y si encuentra
+                    //un objeto en null agrega.
+
+                    e.GetProducto[i] = p;
+                    devuelve = true;
+                    break;
+
+                    }
+                }
+                
+			 
+			}
+            
+            
+           
+            return devuelve;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            //Sustracción (Estante, Producto), retornará un estante 
+            //sin el producto, siempre y cuando el producto se encuentre 
+            //en el listado. Reutilizar código.
+            for (int i = 0; i < e.GetProducto.Length; i++)
+			{
+                if (e == p)
+                {
+                    e.GetProducto[i] = null;
+                }
+                
+			}
+
+            return e;
+
+
+        }
+
+        #endregion
+
+
+        #endregion
+
+    }
+}
---
+++ EjercicioClase5/Producto.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase5
+{
+    class Producto
+    {
+        #region Variables, getters y constructor
+        protected string _codigoDeBarra;
+        protected string _marca;
+        protected float _precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this._marca = marca;
+            this._codigoDeBarra = codigo;
+            this._precio = precio;
+        }
+
+        public string GetMarca { get { return this._marca; } }
+        public float GetPrecio { get { return this._precio; } }
+
+        #endregion
+
+        #region Metodos
+        
+        
+        
+        public static string MostrarProducto(Producto p)
+        {
+           
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Marca: ");
+            sb.AppendLine(p._marca);
+            sb.Append("Codigo de barras: ");
+            sb.AppendLine(p._codigoDeBarra);
+            sb.Append("Precio: ");
+            sb.AppendLine(p._precio.ToString());
+
+            return sb.ToString();
+                
+        }
+
+        #region Sobrecargas
+        public static explicit operator string(Producto p)
+        {
+            //Explícito. Retornará el código de barras del producto que recibe como parámetro.
+            return p._codigoDeBarra;
+
+        }
+       
+        
+        public static bool operator ==(Producto p, string marca)
+        {
+            /*Igualdad (Producto, string). 
+            Retornará true, si la marca del producto coincide con
+            la cadena pasada por parámetro, false, caso contrario.*/
+
+            if (p._marca == marca)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+ 
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            /*Igualdad (Producto, Producto). 
+            Retornará true, si las marcas y códigos de barras son iguales,
+            false, caso contrario.*/
+            if (p1._marca == p2._marca && p1._codigoDeBarra == p2._codigoDeBarra)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+        #endregion
+
+
+        #endregion
+    }
+}
