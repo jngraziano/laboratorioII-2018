0a881611dfae4df372cc1d67ca4a9400e22b38f6
jngraziano <jngraziano@gmail.com>
2018-05-02 17:57:11 -0300

    Inicio ej 40 centralitapolimorfismo

--- CentralTelefonica2/Properties/AssemblyInfo.cs
+++ CentralTelefonica2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ejercicio37-Centralita-Herencia")]
+[assembly: AssemblyTitle("CentralTelefonica")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Ejercicio37-Centralita-Herencia")]
+[assembly: AssemblyProduct("CentralTelefonica")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("89d56907-0d1a-4fec-8e2b-cdaa3304c811")]
+[assembly: Guid("3c9adde5-c516-4871-bbb8-901bc41cfdf4")]
 
 // Version information for an assembly consists of the following four values:
 //
