3942597b1c101ed04b5ad6e466aedf56b04f2f41
jngraziano@gmail.com <jngraziano>
2018-05-07 00:52:30 -0300

    Centralita_Telefonica

--- Centralita_Telefonica/FrmMenu.Designer.cs
+++ Centralita_Telefonica/FrmMenu.Designer.cs
@@ -28,20 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar Llamada";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnGenerar_Llamada);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Facturación Total";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnFactu_Total);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(244, 55);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Facturación Local";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnFactu_Local);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 196);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 55);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Facturación Provincial";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnFactu_Prov);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 257);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(244, 55);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Salir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btnSalir);
             // 
             // FrmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(268, 325);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "FrmMenu";
-            this.Text = "FrmMenu";
+            this.Text = "Central Telefónica";
             this.Load += new System.EventHandler(this.FrmMenu_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
