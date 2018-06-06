ca55cf014e9c78242be7f7a7be3949ff3cbcf8a4
jngraziano <jngraziano@gmail.com>
2018-06-05 22:08:19 -0300

    SQL y threads

---
+++ EjerciciosGuiaClase/Ejercicio60/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio60
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_agregar(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_modificar(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_eliminar(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EjerciciosGuiaClase/Ejercicio60/ProductoDAO.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Ejercicio60
+{
+    public static class ProductoDAO
+    {
+        /*
+         * SQL SERVER 2012 managment studio
+         * Link de descarga de base de datos de ejemmplo
+         * https://github.com/Microsoft/sql-server-samples/releases/tag/adventureworks
+         * 
+         */
+        #region Variables y Constructor
+        private static SqlConnection _conexion;
+        private static SqlCommand _comando;
+
+        /* Original del ppt:
+         * private string connectionStr = "Data Source=[Instancia Del Servidor];Initial Catalog=[Nombre de la Base de Datos];Integrated Security=True";
+                
+         Data Source = es el nombre del servidor en el SQL Server2012
+        
+        */
+        private string connectionStr = "Data Source=.\\SQLEXPRESS;Initial Catalog=AdventureWorks2012;Integrated Security=True";
+
+
+        public ProductoDAO()
+        {
+            //creo la conexion pasandole el string
+            ProductoDAO._conexion = new SqlConnection(connectionStr);
+             // CREO UN OBJETO SQLCOMMAND
+            ProductoDAO._comando = new SqlCommand();
+            // INDICO EL TIPO DE COMANDO
+            ProductoDAO._comando.CommandType = System.Data.CommandType.Text;
+            // ESTABLEZCO LA CONEXION
+            ProductoDAO._comando.Connection = ProductoDAO._conexion;
+
+
+        }
+
+        #endregion
+
+        #region Metodos
+
+
+
+
+        #endregion
+
+
+
+    }
+}
